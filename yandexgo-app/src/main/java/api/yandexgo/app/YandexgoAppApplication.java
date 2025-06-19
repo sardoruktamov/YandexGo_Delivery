0882e6bc90775cd464a3a67424cae30dbe22703d
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-06-19 14:17:40 +0500

    create project

---
+++ yandexgo-app/src/main/java/api/yandexgo/app/YandexgoAppApplication.java
@@ -0,0 +1,13 @@
+package api.yandexgo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexgoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexgoAppApplication.class, args);
+	}
+
+}
---
+++ yandexgo-app/src/test/java/api/yandexgo/app/YandexgoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package api.yandexgo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexgoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
