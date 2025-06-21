036788c745cc9d64874323a4e3a085b23a9fbcf9
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-06-21 21:45:32 +0500

    create GeneralStatus, ProfileRole ENUMS

---
+++ yandexgo-app/src/main/java/api/yandexgo/app/enums/GeneralStatus.java
@@ -0,0 +1,5 @@
+package api.yandexgo.app.enums;
+
+public enum GeneralStatus {
+    ACTIVE, BLOCK, IN_REGISTRATION
+}
---
+++ yandexgo-app/src/main/java/api/yandexgo/app/enums/ProfileRole.java
@@ -0,0 +1,5 @@
+package api.yandexgo.app.enums;
+
+public enum ProfileRole {
+    ROLE_CLIENT, ROLE_DRIVER, ROLE_ADMIN, ROLE_SUPERADMIN
+}
