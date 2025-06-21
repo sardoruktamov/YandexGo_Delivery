136eb66e537c3e7ad532dd959c1998f0ff1cac2f
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-06-21 21:43:50 +0500

    create SpringConfig, AuthController, RegistrationDTO

---
+++ yandexgo-app/src/main/java/api/yandexgo/app/config/SpringConfig.java
@@ -0,0 +1,82 @@
+package api.yandexgo.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+@Configuration
+@EnableWebSecurity
+public class SpringConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        String password = UUID.randomUUID().toString();
+        System.out.println("User Password Mazgi: " + password);
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}" + password)
+                .roles("USER")
+                .build();
+
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(new InMemoryUserDetailsManager(user));
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest()
+                    .authenticated();
+        }).formLogin(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable); // csrf yoqilgan
+        http.cors(httpSecurityCorsConfigurer -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+            configuration.setAllowedMethods(Arrays.asList("*"));
+            configuration.setAllowedHeaders(Arrays.asList("*"));
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            httpSecurityCorsConfigurer.configurationSource(source);
+        }); // cors yoqilgan
+
+        return http.build();
+    }
+
+
+
+
+}
---
+++ yandexgo-app/src/main/java/api/yandexgo/app/controller/AuthController.java
@@ -0,0 +1,25 @@
+package api.yandexgo.app.controller;
+
+
+import api.yandexgo.app.dto.RegistrationDTO;
+import api.yandexgo.app.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@Slf4j
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/registration")
+    public ResponseEntity<String> registration(@Valid @RequestBody RegistrationDTO dto){
+        return ResponseEntity.ok().body(authService.registration(dto));
+    }
+
+}
