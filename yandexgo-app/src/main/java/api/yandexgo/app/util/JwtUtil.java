ed3484bad0d4773b4fbba61719a5f5575b131e8a
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-06-28 12:42:04 +0500

    create JwtUtil for login

--- yandexgo-app/src/main/java/api/yandexgo/app/util/JwtUtil.java
+++ yandexgo-app/src/main/java/api/yandexgo/app/util/JwtUtil.java
@@ -16,17 +16,50 @@ public class JwtUtil {
     private static final int tokenLiveTime = 1000 * 3600 * 24; // 1-day
     private static final String secretKey = "veryLongSecretkalitbuagarbunioqimoqchibolsangizsizaqldanozganizsizgamaslahatmbunioqimangchunkibumatnhechnimanianglatmaydiagaroqisangizoxiridameniqargamangmazgi";
 
-    public static String encode(Integer id) {
-//    public static String encode(String username, Integer id, List<ProfileRole> roleList) {
-//
-//        String strRoleList = roleList.stream()
-//                .map(item -> item.name())
-//                .collect(Collectors.joining(","));  // "ROLE_USER,ROLE_ADMIN"
-//
-//        Map<String,String> claims = new HashMap<>();
-//        claims.put("roles", strRoleList);
-//        claims.put("id", String.valueOf(id));
+    public static String encode(String username, Integer id, List<ProfileRole> roleList) {
 
+        String strRoleList = roleList.stream()
+                .map(item -> item.name())
+                .collect(Collectors.joining(","));  // "ROLE_USER,ROLE_ADMIN"
+
+        Map<String,String> claims = new HashMap<>();
+        claims.put("roles", strRoleList);
+        claims.put("id", String.valueOf(id));
+
+        return Jwts
+                .builder()
+                .subject(String.valueOf(id))
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + tokenLiveTime))
+            .signWith(getSignInKey())
+            .compact();
+    }
+
+    public static JwtDTO decode(String token) {
+        Claims claims = Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+         String username = claims.getSubject();
+         Integer id = Integer.valueOf((String) claims.get("id"));
+         String strRole = (String) claims.get("roles");  // "ROLE_USER,ROLE_ADMIN"
+         // 1-oddiyroq usul
+         //        String[] roleArray = strRole.split(",");
+         //        List<ProfileRole> roleList_1 = new ArrayList<>();
+         //        for (String role : roleArray) {
+         //            roleList_1.add(ProfileRole.valueOf(role));
+         //        }
+         // 2-qisqaroq usul
+         List<ProfileRole> roleList_2 = Arrays.stream(strRole.split(","))
+         .map(ProfileRole::valueOf)
+         .toList();
+         return new JwtDTO(username, id, roleList_2);
+//        return Integer.valueOf(claims.getSubject());
+
+    }
+    public static String encode(Integer id) {
         return Jwts
                 .builder()
                 .subject(String.valueOf(id))
@@ -36,27 +69,13 @@ public class JwtUtil {
                 .compact();
     }
 
-    public static Integer decodeVer(String token) {
+    public static Integer decodeRegVer(String token) {
         Claims claims = Jwts
                 .parser()
                 .verifyWith(getSignInKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
-       /** String username = claims.getSubject();
-        Integer id = Integer.valueOf((String) claims.get("id"));
-        String strRole = (String) claims.get("roles");  // "ROLE_USER,ROLE_ADMIN"
-        // 1-oddiyroq usul
-//        String[] roleArray = strRole.split(",");
-//        List<ProfileRole> roleList_1 = new ArrayList<>();
-//        for (String role : roleArray) {
-//            roleList_1.add(ProfileRole.valueOf(role));
-//        }
-        // 2-qisqaroq usul
-        List<ProfileRole> roleList_2 = Arrays.stream(strRole.split(","))
-                .map(ProfileRole::valueOf)
-                .toList();
-        return new JwtDTO(username, id, roleList_2); **/
         return Integer.valueOf(claims.getSubject());
 
     }
