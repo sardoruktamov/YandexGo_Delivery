f3dfc41ea986d3c7170b8c427ade1dc9933bbe64
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-06-28 12:40:35 +0500

    create JwtDTO for login

--- yandexgo-app/src/main/java/api/yandexgo/app/dto/JwtDTO.java
+++ yandexgo-app/src/main/java/api/yandexgo/app/dto/JwtDTO.java
@@ -14,7 +14,38 @@ import java.util.List;
 @NoArgsConstructor
 public class JwtDTO {
 
+
     private String username;
     private Integer id;
     private List<ProfileRole> roleList;
+
+    public JwtDTO(String username, Integer id, List<ProfileRole> roleList) {
+        this.username = username;
+        this.id = id;
+        this.roleList = roleList;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<ProfileRole> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<ProfileRole> roleList) {
+        this.roleList = roleList;
+    }
 }
